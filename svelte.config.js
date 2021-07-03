5ca105d319cbe48a03a3fddae2c62fe1cbef6e35
spathak19 <sppathak28@gmail.com>
2021-07-03 23:02:41 +0530

    added node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
+import node from '@sveltejs/adapter-node';
 const config = {
 	kit: {
+		adapter: node(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
